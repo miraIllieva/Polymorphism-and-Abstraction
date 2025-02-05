1756cd2c508da9647ade561efa1ea5a0a1958757
Myra Ilieva <159176371+miraIllieva@users.noreply.github.com>
2025-02-04 22:19:22 -0500

    Add files via upload

---
+++ groups.py
@@ -0,0 +1,48 @@
+class Person:
+    def __init__(self, name: str, surname: str):
+        self.name = name
+        self.surname = surname
+
+    def __str__(self):
+        return f"{self.name} {self.surname}"
+
+    def __add__(self, other):
+        return Person(self.name, other.surname)
+
+
+class Group:
+    def __init__(self, name: str, people: list):
+        self.name = name
+        self.people = people
+
+    def __len__(self):
+        return len(self.people)
+
+    def __add__(self, other):
+        return Group(f"{self.name} {other.name}", self.people + other.people)
+
+    def __str__(self):
+        return f"Group {self.name} with members {', '.join(str(p) for p in self.people)}"
+
+    def __getitem__(self, index):
+        return f"Person {index}: {self.people[index]}"
+
+
+# Test cases
+p0 = Person("Aliko", "Dangote")
+p1 = Person("Bill", "Gates")
+p2 = Person("Warren", "Buffet")
+p3 = Person("Elon", "Musk")
+p4 = p2 + p3
+
+first_group = Group("__VIP__", [p0, p1, p2])
+second_group = Group("Special", [p3, p4])
+
+third_group = first_group + second_group
+
+print(len(first_group))  # Expected: 3
+print(second_group)  # Expected: Group Special with members Elon Musk, Warren Musk
+print(third_group[0])  # Expected: Person 0: Aliko Dangote
+
+for person in third_group:
+    print(person)
